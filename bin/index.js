10fd0520b37e15eff9374567f8d5ebb14ff911f2
dowonee <sssjdw@naver.com>
2025-03-12 02:20:31 +0900

    feat: CLI 기본 구조 설정 (#4)

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,35 @@ import { program } from "commander";
 
 program
   .version("1.0.0")
-  .name("snapbug");
+  .name("snapbug")
+  .description("명령어 한 줄로 배포하고, 링크 하나로 팀원과 실시간으로 상태를 공유하세요!");
+
+program
+  .command("start")
+  .description("디버깅 모드를 시작합니다. 상태 기록을 활성화합니다.")
+  .action(() => {
+    console.log("상태 기록 중 입니다.");
+  });
+
+program
+  .command("end")
+  .description("디버깅 모드를 종료합니다. 상태 기록을 중단합니다.")
+  .action(() => {
+    console.log("상태 기록을 중단했습니다.");
+  });
+
+program
+  .command("run")
+  .description("현재 프로젝트를 Vercel를 통해 배포합니다.")
+  .action(() => {
+    console.log("프로젝트가 배포되었습니다. URL: [URL]");
+  });
+
+program
+  .command("delete")
+  .description("배포된 프로젝트를 삭제합니다.")
+  .action(() => {
+    console.log("배포된 프로젝트가 삭제되었습니다.");
+  });
 
 program.parse(process.argv);
