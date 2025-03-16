4efb285d4bc764f53ec897e3264c3ee58c0fbc5f
dowonee <sssjdw@naver.com>
2025-03-17 01:35:09 +0900

    feat: Puppeteer에서 리액트 페이지 열기 기능 구현

--- src/puppeteer/browser.js
+++ src/puppeteer/browser.js
@@ -8,3 +8,10 @@ export const openBrowser = async () => {
 
   return browser;
 };
+
+export const getReactPage = async (browser) => {
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000", { waitUntil: "networkidle0" });
+
+  return page;
+};
