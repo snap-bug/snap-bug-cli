488478d8ce3c50a1dd19f5dd3f67528ceac215ed
dowonee <jjddwwyt@gmail.com>
2025-03-17 02:56:33 +0900

    refactor: 개발 서버 페이지 이동 시 예외처리 추가

--- src/puppeteer/browser.js
+++ src/puppeteer/browser.js
@@ -11,7 +11,14 @@ export const openBrowser = async () => {
 
 export const getReactPage = async (browser) => {
   const page = await browser.newPage();
-  await page.goto("http://localhost:3000", { waitUntil: "networkidle0" });
+
+  try {
+    await page.goto("http://localhost:3000", { waitUntil: "networkidle0" });
+
+    return page;
+  } catch (error) {
+    console.error("개발 서버에 접속할 수 없습니다.", error);
+  }
 
   return page;
 };
