be66846d256c6543b98df0b4d87fc182899c3f3c
dowonee <jjddwwyt@gmail.com>
2025-03-20 16:14:46 +0900

    feat: 상태 변경시 파일에 저장하는 기능 추가

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,6 +1,11 @@
 import fs from "fs/promises";
+import path from "path";
+import { v4 as uuidv4 } from "uuid";
+import config from "./config.js";
 
-export async function fileExists(filePath) {
+const STATE_FILE = path.resolve(config.STATE_FILE_PATH);
+
+async function fileExists(filePath) {
   try {
     await fs.access(filePath);
     return true;
@@ -8,3 +13,36 @@ export async function fileExists(filePath) {
     return false;
   }
 }
+
+export async function saveStateToFile(newEntry) {
+  try {
+    let existingData = [];
+
+    if (await fileExists(STATE_FILE)) {
+      try {
+        const fileData = await fs.readFile(STATE_FILE, "utf-8");
+
+        existingData = fileData ? JSON.parse(fileData) : [];
+      } catch (error) {
+        console.error("파일 읽기를 실패했습니다.", error);
+      }
+
+      const lastEntry = existingData[existingData.length - 1];
+
+      if (!newEntry.dom && lastEntry?.dom) {
+        newEntry.dom = lastEntry.dom;
+      }
+    }
+
+    newEntry.id = uuidv4();
+    existingData.push(newEntry);
+
+    await fs.writeFile(STATE_FILE, JSON.stringify(existingData, null, config.JSON_INDENTATION));
+    console.log("파일 저장에 성공했습니다.", newEntry);
+
+    return existingData;
+  } catch (err) {
+    console.error("파일 저장을 실패했습니다.", err);
+  }
+}
+
