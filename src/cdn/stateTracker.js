ca19c6f5aaa1cd66134fdd9b0bcf9d2fe52d7443
dowonee <jjddwwyt@gmail.com>
2025-03-25 12:51:56 +0900

    refactor: 해시 계산 함수 내 매직 넘버 상수화 및 변수명 개선

--- src/cdn/stateTracker.js
+++ src/cdn/stateTracker.js
@@ -93,12 +93,14 @@ const traverseFiberTree = (fiberNode) => {
 };
 
 const getDOMHash = async (domString) => {
+  const HASH_RADIX = 16;
+  const HASH_PAD_LENGTH = 2;
   const encoder = new TextEncoder();
   const data = encoder.encode(domString);
   const hashBuffer = await crypto.subtle.digest("SHA-256", data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
 
-  return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+  return hashArray.map((byte) => byte.toString(HASH_RADIX).padStart(HASH_PAD_LENGTH, "0")).join("");
 };
 
 const detectStateChange = async () => {
