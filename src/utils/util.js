cca90e2cc1d945acc34d894899ef4d1f5bb5320a
sekyeong <sekoonglee@gmail.com>
2025-03-26 00:33:32 +0900

    feat: 명령 실행 유틸 runCommand 함수 추가

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,3 +1,25 @@
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+import { spawn } from "child_process";
 
-export default { delay };
+export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const runCommand = (cmd, args = [], options = {}) => {
+  return new Promise((resolve, reject) => {
+    const child = spawn(cmd, args, {
+      stdio: "inherit",
+      shell: true,
+      ...options,
+    });
+
+    child.on("exit", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`${cmd} 종료 코드 : ${code}`));
+      }
+    });
+
+    child.on("error", (err) => {
+      reject(new Error(`${cmd} 실행 실패: ${err.message}`));
+    });
+  });
+};
