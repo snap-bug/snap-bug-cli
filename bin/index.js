9b356d5700701742ea50f0d54d59da62631305bd
sekyeong <sekoonglee@gmail.com>
2025-03-27 00:52:37 +0900

    chore: run 명령어 client-path 기본값 경로 수정

--- bin/index.js
+++ bin/index.js
@@ -11,7 +11,7 @@ program
 program
   .command("run")
   .description("디버깅 UI를 빌드하고 Vercel을 통해 배포합니다.")
-  .option("--client-path <path>", "snapbug-client 경로", "../snapbug-client")
+  .option("--client-path <path>", "snapbug-client 경로", "../snap-bug-client")
   .option("--deploy", "Vercel에 자동 배포합니다.", true)
   .action(async (options) => {
     await run({
