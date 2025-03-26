f79b8c2df15238770dcf7ac783b5616ceb849c50
정도원 <83629099+dowonee@users.noreply.github.com>
2025-03-27 08:29:16 +0900

    fix: 상태 JSON 저장 시 오류 해결 (#53) (#54)
    * refactor: 상태 저장 시 styles 추가
    
    * refactor: 요청 body 크기 제한을 50MB로 상향
    
    * refactor: 임시로 모든 도메인 접근 허용

--- src/server/server.js
+++ src/server/server.js
@@ -5,8 +5,8 @@ import httpStatusCode from "../utils/httpStatusCode.js";
 import { getStateById, getStateHistory, saveStateToFile } from "../utils/fileUtils.js";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(cors({ origin: "*" }));
 
 app.get("/states", async (req, res) => {
   try {
@@ -43,13 +43,13 @@ app.get("/states/:id", async (req, res) => {
 
 app.post("/states", async (req, res) => {
   try {
-    const { timestamp, state, dom } = req.body;
+    const { timestamp, state, dom, styles } = req.body;
 
     if (!timestamp || !state) {
       return res.status(httpStatusCode.BAD_REQUEST).json({ errorMessage: "Bad Request" });
     }
 
-    const updatedHistory = await saveStateToFile({ timestamp, state, dom });
+    const updatedHistory = await saveStateToFile({ timestamp, state, dom, styles });
 
     if (!updatedHistory) {
       return res.status(httpStatusCode.NOT_FOUND).json({ errorMessage: "저장할 상태가 없습니다" });
