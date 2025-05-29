d97ab771a6c877a9057a06588228e0dd80c2578e
Dowon <83629099+dowonee@users.noreply.github.com>
2025-05-29 20:51:25 +0900

    feat: 상태 기록 시 첫 기록이면 초기화 후 저장되도록 로직 개선 (#59) (#60)
    * style: 상태 추적 json 파일명 수정
    
    * chore: gitignore 수정
    
    * feat: 상태 기록 시 첫 기록이면 초기화 후 저장되도록 로직 개선

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,13 +4,13 @@ const PUBLIC_DIR = path.resolve(process.cwd(), "public");
 
 const config = {
   PUBLIC_DIR,
-  STATE_FILE_NAME: "snapbug-data.json",
+  STATE_FILE_NAME: "snapbug-state.json",
   USER_DEV_URL: "http://localhost:5173",
   API_SERVER_URL: "http://localhost:3001",
   API_SERVER_PORT: 3001,
   JSON_INDENTATION: 2,
   WAIT_TIME: 5000,
-  STATE_FILE_PATH: path.join(PUBLIC_DIR, "snapbug-data.json"),
+  STATE_FILE_PATH: path.join(PUBLIC_DIR, "snapbug-state.json"),
 };
 
 export default config;
